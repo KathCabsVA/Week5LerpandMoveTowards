998f9ded952dc12edf56118750319e5b35220972
KathCabsVA <155535379+KathCabsVA@users.noreply.github.com>
2024-05-31 17:33:34 +0800

    updated the move towards script by dyep
    -added x and y axis for movement if it gets followed
    -installed DOTween package from MyAssets
    -added follow in inspector to check if false or true
    -added a followe target then insert gameobject there

--- Assets/MoveTowards.cs
+++ Assets/MoveTowards.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 
 public class MoveTowards : MonoBehaviour
@@ -7,6 +8,9 @@ public class MoveTowards : MonoBehaviour
     public float magnitude, speed;
     public Vector2 startPosition;
 
+    public Transform followTarget;
+
+    public bool follow = false;
 
     void Start()
     {
@@ -16,8 +20,13 @@ public class MoveTowards : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //transform.position = new Vector2(0, LerpMovement());
-        transform.position = new Vector2(-3, MoveTowardsMovement());
+        if (follow)
+        {
+            //transform.position = new Vector2(0, LerpMovement());
+            transform.position = new Vector2(MoveTowardsMovementXAxis(), MoveTowardsMovementYAxis());
+            return;
+        }
+        transform.position = new Vector2(startPosition.x, LerpMovement());
     }
 
     private void OnDrawGizmos()
@@ -43,9 +52,15 @@ public class MoveTowards : MonoBehaviour
     }
 
 
-    public float MoveTowardsMovement()
+    public float MoveTowardsMovementYAxis()
     {
-        return Mathf.MoveTowards(startPosition.y, startPosition.y + magnitude, Mathf.PingPong(Time.time * speed, magnitude));
-
+        return Mathf.MoveTowards(this.transform.position.y, followTarget.position.y, Mathf.PingPong(Time.deltaTime * speed, magnitude));
     }
+
+    public float MoveTowardsMovementXAxis()
+    {
+        return Mathf.MoveTowards(this.transform.position.x, followTarget.position.x, Mathf.PingPong(Time.deltaTime * speed, magnitude));
+    }
+
+
 }
